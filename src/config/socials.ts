60791e1f9c085fbf69bb8c7f3e25f963addebe30
Abhinav Thakur <thakurabhinav16160@gmail.com>
2026-06-23 22:14:55 +0530

    feat: add centralized socials config file

---
+++ src/config/socials.ts
@@ -0,0 +1,6 @@
+export const socials = {
+  github: "https://github.com/Abhinavthakur20",
+  linkedin: "https://www.linkedin.com/in/abhinav-thakur-b4a1b023b",
+  twitter: "https://twitter.com/abhinavthakur_d",
+  email: "mailto:abhinav.thakur.work@gmail.com",
+};
